a0d7864a0b7070d7884bb0062860c03f6f985fa5
ukachar <83542678+ukachar@users.noreply.github.com>
2024-04-16 14:37:12 +0200

    +

--- client/src/components/test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function test() {
-    let a = ['da', 'ne', 'bla', 'kre'];
-    return a;
-
-}
---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import "./App.css";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
+function App() {
+  return (
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
+}
+
+export default App;
